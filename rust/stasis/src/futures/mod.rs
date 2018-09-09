b3739e1ec46a31e66c61576fdf3ee916c4aa1a86
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2018-09-09 14:30:41 -0400

    Add note on poll order for futures

--- rust/stasis/src/futures/mod.rs
+++ rust/stasis/src/futures/mod.rs
@@ -1,4 +1,14 @@
 //! Support for various versions of `futures`.
+//!
+//! The executors in this module will never fail to spawn a future, barring
+//! extreme circumstances such as OOM errors.
+//!
+//! ## A note on poll order
+//!
+//! Futures can spawn additional futures while they are themselves being
+//! polled. The implementations here do not use a queue to handle this
+//! situation, rather they immediately poll the freshly spawned future. This
+//! should not affect usage of futures.
 
 pub mod v01;
 pub mod v02;
